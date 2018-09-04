990adb8e2610b43c6723e40e12e9eb332a0a9dd5
Nazar Moroziuk <37048797+morozyuk@users.noreply.github.com>
2018-09-04 20:43:58 +0300

    Add files via upload

---
+++ FE_9_11_homework_dom/homework/src/app.js
@@ -0,0 +1,91 @@
+// var rootNode = document.getElementById("root");
+//
+// // Your code goes here
+//
+// rootNode.appendChild(/* Append your list item node*/);
+let node = document.createElement('LI');
+let textnode = document.createTextNode('Water');
+node.appendChild(textnode);
+document.getElementById('list').appendChild(node);
+let input=document.getElementById('input');
+let buttonAdd=document.getElementById('buttonAdd');
+let counter = 0;
+let ten =10;
+let eleven =11;
+buttonAdd.onclick =function (){
+    if(input.value ===''){
+        buttonAdd.disabled;
+        input.value = '';
+    }else if (counter < ten) {
+        counter ++;
+        let li = document.createElement('li');
+        li.draggable = true;
+        li.className = 'default_filter';
+        let textnode = document.createTextNode(input.value);
+        let iconCheck = document.createElement('i');
+        let iconDelete = document.createElement('i');
+        textnode.className = 'textIn';
+        iconDelete.innerText = 'delete';
+        iconDelete.className = 'material-icons iconDelete';
+        iconCheck.className = 'material-icons';
+        iconCheck.innerText = 'check_box_outline_blank';
+        li.appendChild(iconCheck);
+        li.appendChild(textnode);
+        li.appendChild(iconDelete);
+        document.getElementById('list').append(li);
+        console.log(counter);
+        iconCheck.onclick = function () {
+            iconCheck.innerText = 'check_box';
+        };
+        iconDelete.onclick = function () {
+            li.remove();
+            counter --;
+        };
+        input.value = '';
+        let dragging = null;
+        let ulOur = document.getElementById('list');
+        ulOur.addEventListener('dragstart', event => {
+            dragging = event.target;
+        });
+        ulOur.addEventListener('dragover', event => {
+            if (event.target.className === 'default_filter') {
+                event.preventDefault();
+                let zero = 0;
+                let half = 2;
+                let bounding = event.target.getBoundingClientRect();
+                let set = bounding.y + bounding.height / half;
+                if (event.clientY - set > zero) {
+                    event.target.style['border-top'] = '';
+                    event.target.style['border-bottom'] = '1px dashed #ccc';
+                } else {
+                    event.target.style['border-top'] = '1px dashed #ccc';
+                    event.target.style['border-bottom'] = '';
+                }
+            }
+        });
+        ulOur.addEventListener('dragleave', event => {
+            event.target.style['border-bottom'] = '';
+            event.target.style['border-top'] = '';
+        });
+        ulOur.addEventListener('drop', event => {
+            if (event.target.className === 'default_filter') {
+                event.preventDefault();
+                if (event.target.style['border-bottom']) {
+                    event.target.style['border-bottom'] = '';
+                    ulOur.insertBefore(dragging, event.target.nextSibling);
+                } else {
+                    event.target.style['border-top'] = '';
+                    ulOur.insertBefore(dragging, event.target);
+                }
+            }
+        });
+    }else if(counter === ten){
+        counter = eleven;
+        buttonAdd.disabled;
+        input.value = '';
+        let newElement = document.getElementById('newEl');
+        newElement.style.display = 'flex';
+
+    }
+    input.value = '';
+};
